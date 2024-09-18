e6da07f39d7a91b49c0f6f72ae86ef3d47f8ca45
JoseBrouwer <jbb20b@fsu.edu>
2024-09-18 16:26:26 -0400

    Handled errors when user cancels the file explorer windows

--- find_links.py
+++ find_links.py
@@ -54,133 +54,139 @@ def validate_json_file(file_path):
         raise ValueError("Unsupported file type. Please select a JSON file.")
 
 def folders():
-    with open(output_file, "w", encoding="utf-8") as outfile:
-        for folder in os.listdir(content_directory):
-            #print("in folder: " + folder)
-            folderPath = content_directory + "/" + folder         
-            if os.path.isdir(folderPath):            
-                for filename in os.listdir(folderPath):                 
-                    if filename.endswith(".html"):
-                        #print("reading file: " + filename)
-                        filepath = os.path.join(folderPath, filename)
-                        with open(filepath, 'r', encoding='utf-8') as html_file:
-                            soup = BeautifulSoup(html_file, 'html.parser')
-                            for a_tag in soup.find_all('a', href=True):
-                                url = str(a_tag['href'])
+    if(content_directory != ""):
+        with open(output_file, "w", encoding="utf-8") as outfile:
+            for folder in os.listdir(content_directory):
+                #print("in folder: " + folder)
+                folderPath = content_directory + "/" + folder         
+                if os.path.isdir(folderPath):            
+                    for filename in os.listdir(folderPath):                 
+                        if filename.endswith(".html"):
+                            #print("reading file: " + filename)
+                            filepath = os.path.join(folderPath, filename)
+                            with open(filepath, 'r', encoding='utf-8') as html_file:
+                                soup = BeautifulSoup(html_file, 'html.parser')
+                                for a_tag in soup.find_all('a', href=True):
+                                    url = str(a_tag['href'])
 
-                                outfile.write(f"{filename}\n")
-                                outfile.write(f"{url}\n")
-                                #outfile.write(f"Tags: {tags}\n")
-                                outfile.write("--\n")
+                                    outfile.write(f"{filename}\n")
+                                    outfile.write(f"{url}\n")
+                                    #outfile.write(f"Tags: {tags}\n")
+                                    outfile.write("--\n")
 
 def no_folders():
-    with open(output_file, "w") as outfile:
-        for filename in os.listdir(content_directory):
-            if filename.endswith(".html"):
-                unit_number = "unit" + filename.split('.')[0][0]
-                filepath = os.path.join(content_directory, filename)
-                
-                with open(filepath, 'r', encoding='utf-8') as html_file:
-                    soup = BeautifulSoup(html_file, 'html.parser')
-                    for a_tag in soup.find_all('a', href=True):
-                        url = str(a_tag['href'])
+    if(content_directory != ""):
+        with open(output_file, "w") as outfile:
+            for filename in os.listdir(content_directory):
+                if filename.endswith(".html"):
+                    unit_number = "unit" + filename.split('.')[0][0]
+                    filepath = os.path.join(content_directory, filename)
+                    
+                    with open(filepath, 'r', encoding='utf-8') as html_file:
+                        soup = BeautifulSoup(html_file, 'html.parser')
+                        for a_tag in soup.find_all('a', href=True):
+                            url = str(a_tag['href'])
 
-                        outfile.write(f"{filename}\n")
-                        outfile.write(f"{url}\n")
-                        #outfile.write(f"Tags: {tags}\n")
-                        outfile.write("--\n")
+                            outfile.write(f"{filename}\n")
+                            outfile.write(f"{url}\n")
+                            #outfile.write(f"Tags: {tags}\n")
+                            outfile.write("--\n")
 
 def uses_api():
-    all_ids = {}
-    with open(output_file, "w") as outfile:
-        for filename in os.listdir(content_directory):
-            if filename.endswith(".html"):
-                filepath = os.path.join(content_directory, filename)
-                file_ids = get_linkIds(filepath)
-                all_ids[str(filename)] = file_ids # set key's (the file's) value to a list of ids
-        
-        print("Done getting IDs")
-        links = search_json()
-        
-        # Iterate over each filename and its associated IDs
-        for filename, file_ids in all_ids.items():
-            for file_id in file_ids:
-                str_file_id = str(file_id)
-                # Search for the file_id in the links from the JSON file
-                for link in links["links"]:
-                    if link["linkId"] == str_file_id:
-                        url = link["url"]
-                        tags = link["tags"]
-                        # Write the filename along with the other link data to the output file
-                        outfile.write(f"{filename}\n")
-                        outfile.write(f"{url}\n")
-                        #outfile.write(f"Tags: {tags}\n")
-                        outfile.write("--\n")
-    print("Done Writing for API")
+    if(content_directory != ""):
+        all_ids = {}
+        with open(output_file, "w") as outfile:
+            for filename in os.listdir(content_directory):
+                if filename.endswith(".html"):
+                    filepath = os.path.join(content_directory, filename)
+                    file_ids = get_linkIds(filepath)
+                    all_ids[str(filename)] = file_ids # set key's (the file's) value to a list of ids
+            
+            print("Done getting IDs")
+            links = search_json()
+            
+            # Iterate over each filename and its associated IDs
+            for filename, file_ids in all_ids.items():
+                for file_id in file_ids:
+                    str_file_id = str(file_id)
+                    # Search for the file_id in the links from the JSON file
+                    for link in links["links"]:
+                        if link["linkId"] == str_file_id:
+                            url = link["url"]
+                            tags = link["tags"]
+                            # Write the filename along with the other link data to the output file
+                            outfile.write(f"{filename}\n")
+                            outfile.write(f"{url}\n")
+                            #outfile.write(f"Tags: {tags}\n")
+                            outfile.write("--\n")
+        print("Done Writing for API")
           
 def get_linkIds(filepath):
-    values = []
-    #Get a list of ids to search in links.json
-    with open(filepath, 'r', encoding='utf-8') as html_file:
-        soup = BeautifulSoup(html_file, 'html.parser')
-        
-        #soup.find_all returns a list of HTML elements matching <span data-linkApi-id...>
-        #span_tag is an entry in this list which I treat as a string
-        for span_tag in soup.find_all('span'):
-            tag_string = str(span_tag)
-            value_string = ""
-
-            value_location = tag_string.find('"') + 1 #first index of digit
-            for char in tag_string[value_location:]:
-                if char == '"' or not char.isdigit():
-                    break
-                elif char.isdigit():
-                    value_string = value_string + char
+    if(content_directory != ""):
+        values = []
+        #Get a list of ids to search in links.json
+        with open(filepath, 'r', encoding='utf-8') as html_file:
+            soup = BeautifulSoup(html_file, 'html.parser')
             
-            if(value_string.isdigit()):
-                value = int(value_string) #cast to int
-                values.append(value) #add to list
-    return values
+            #soup.find_all returns a list of HTML elements matching <span data-linkApi-id...>
+            #span_tag is an entry in this list which I treat as a string
+            for span_tag in soup.find_all('span'):
+                tag_string = str(span_tag)
+                value_string = ""
+
+                value_location = tag_string.find('"') + 1 #first index of digit
+                for char in tag_string[value_location:]:
+                    if char == '"' or not char.isdigit():
+                        break
+                    elif char.isdigit():
+                        value_string = value_string + char
+                
+                if(value_string.isdigit()):
+                    value = int(value_string) #cast to int
+                    values.append(value) #add to list
+        return values
 
 def search_json():
-    print("Select the links2.json file in the data folder:")
-    file_path = get_file_path(True)
-    json_path = validate_json_file(file_path)
-    
-    with open(json_path, 'r', encoding='utf-8') as json_file:
-        links = json.load(json_file)
-        print("Done retrieving JSON contents")
-        return links    
+    if(content_directory != ""):
+        print("Select the JSON file in this program's folder:")
+        file_path = get_file_path(True)
+        json_path = validate_json_file(file_path)
+        
+        with open(json_path, 'r', encoding='utf-8') as json_file:
+            links = json.load(json_file)
+            print("Done retrieving JSON contents")
+            return links    
 
 def parse_extracted():
-    with open("extracted_links.txt", "r", encoding="utf-8", errors="replace") as file:
-        lines = file.readlines()
+    if(content_directory != ""):
+        with open("extracted_links.txt", "r", encoding="utf-8", errors="replace") as file:
+            lines = file.readlines()
 
-    # Process each link and store in a list in Mark's format
-    links = []
-    current_link = []
+        # Process each link and store in a list in Mark's format
+        links = []
+        current_link = []
 
-    for line in lines:
-        if line.strip() == "--":  # End of a link entry
-            if current_link:
-                file = current_link[0]
-                url = current_link[1]
-                link = {
-                    "file": file,
-                    "url": url
-                }
-                links.append(link)
-                current_link = []  # Reset for the next link
-        else:
-            if line.strip():  # Add non-empty lines to current link
-                current_link.append(line.strip())
+        for line in lines:
+            if line.strip() == "--":  # End of a link entry
+                if current_link:
+                    file = current_link[0]
+                    url = current_link[1]
+                    link = {
+                        "file": file,
+                        "url": url
+                    }
+                    links.append(link)
+                    current_link = []  # Reset for the next link
+            else:
+                if line.strip():  # Add non-empty lines to current link
+                    current_link.append(line.strip())
 
-    # Create a dictionary to store the links
-    data = {"links": links}
-    # Write the data to a JSON file in the 'data' directory
-    with open("links2.json", "w", encoding="utf-8") as json_file:
-        json.dump(data, json_file, indent=4)
-    os.remove("extracted_links.txt")
+        # Create a dictionary to store the links
+        data = {"links": links}
+        # Write the data to a JSON file in the 'data' directory
+        with open("links2.json", "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, indent=4)
+        os.remove("extracted_links.txt")
 
 if __name__ == "__main__":
     if(len(sys.argv) < 2):
